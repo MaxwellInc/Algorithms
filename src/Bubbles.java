d6e1df323ebbf29b9da9da6aff0685b76453413a
Eugene <zhek0nd98@gmail.com>
2019-01-12 14:10:34 +0200

    Counting comparisons of array elements

--- src/Bubbles.java
+++ src/Bubbles.java
@@ -4,13 +4,14 @@ public class Bubbles {
     static int[] arr = new int[]{6, -4, 2, 0, 12, -543, 553};
     static int numberOfIterations = 0;
     static int numberOfSwaps = 0;
+    static int numberOfComparisons = 0;
 
     public static void main(String[] args) {
 
         for (int i = 0; i < arr.length; i++) {
             numberOfIterations++;
             for (int j = 0; j < arr.length - 1; j++) {
-                if (arr[j] > arr[j + 1]) {
+                if (compareValues(j, j + 1)) {
                     int temp = arr[j + 1];
                     arr[j + 1] = arr[j];
                     arr[j] = temp;
@@ -26,7 +27,7 @@ public class Bubbles {
         for (int i = 0; i < arr.length; i++) {
             numberOfIterations++;
             for (int j = i; j < arr.length - 1; j++) {
-                if (arr[i] > arr[j + 1]) {
+                if (compareValues(i, j + 1)) {
                     int temp = arr[j + 1];
                     arr[j + 1] = arr[i];
                     arr[i] = temp;
@@ -41,7 +42,7 @@ public class Bubbles {
 
         for (int i = 0; i < arr.length; i++) {
             numberOfIterations++;
-            for (int j = i; j > 0 && arr[j - 1] > arr[j]; numberOfIterations++) {
+            for (int j = i; j > 0 && compareValues(j-1, j); numberOfIterations++) {
                 int temp = arr[j];
                 arr[j] = arr[j - 1];
                 arr[j - 1] = temp;
@@ -58,6 +59,7 @@ public class Bubbles {
     static void reset() {
         numberOfSwaps = 0;
         numberOfIterations = 0;
+        numberOfComparisons = 0;
         arr = new int[]{6, -4, 2, 0, 12, -543, 553};
     }
 
@@ -65,7 +67,11 @@ public class Bubbles {
         for (int i : arr) {
             System.out.println(i);
         }
-        System.out.println("Iterations : " + numberOfIterations + "\nSwaps : " + numberOfSwaps);
+        System.out.println("\nIterations : " + numberOfIterations + "\nSwaps : " + numberOfSwaps + "\nComparisons : "
+        + numberOfComparisons + "\n-----------\n");
     }
-
+    static boolean compareValues(int indexOne, int indexTwo){
+        numberOfComparisons++;
+        return arr[indexOne] > arr[indexTwo];
+    }
 }
